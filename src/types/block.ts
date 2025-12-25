fee037d11e9587d4756d0d63e76dea09ad6adf5a
Moamal Hussin <dev.moamal@gmail.com>
2025-12-25 10:16:12 +0300

    create block types

---
+++ src/types/block.ts
@@ -0,0 +1,15 @@
+export type BlockType = "Text" | "Heading";
+
+export type TextColor = `#${string}`; // Hex color ex : #ffffff
+
+export type BlockProps = {
+  textSize?: number; // pixel value ex : 10 that means 10px
+  textColor?: TextColor;
+};
+
+export type Block = {
+  id: string;
+  type: BlockType;
+  props?: BlockProps;
+  content: string;
+};
