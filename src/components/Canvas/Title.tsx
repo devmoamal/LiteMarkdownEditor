1f5de01303a81b14325d2197fc584743e41dffd8
Moamal Hussin <dev.moamal@gmail.com>
2025-12-26 16:36:26 +0300

    Add Title component for managing document title input

---
+++ src/components/Canvas/Title.tsx
@@ -0,0 +1,20 @@
+import useEditor from "@/hooks/useEditor";
+import { cn } from "@/lib/utils";
+
+type TitleProps = {
+  className?: string;
+};
+
+function Title({ className }: TitleProps) {
+  const { title, setTitle } = useEditor();
+  return (
+    <input
+      className={cn("text-2xl font-bold", className)}
+      value={title}
+      onChange={(e) => setTitle(e.target.value)}
+      placeholder="Title"
+    />
+  );
+}
+
+export default Title;
